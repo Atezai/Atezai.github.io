8eb618f7a144765bb24112ed57a359db19276860
Atezai <theatezai@gmail.com>
2019-01-20 23:13:43 +0100

    aboutme s

---
+++ atezai_files/paralaxStars/paralax.js
@@ -0,0 +1,17 @@
+var paralaxObjects = document.getElementsByClassName("paralaxStars");
+
+InitializeParalaxEffect();
+
+function InitializeParalaxEffect(){
+  for(var i = 0; i < paralaxObjects.length; i++){
+    paralaxObjects[i].style.backgroundPositionX = i*i*500 + "px";
+    paralaxObjects[i].style.opacity = 1 - i/9;
+  }
+  ParalaxEffect();
+}
+
+function ParalaxEffect(){
+  for(var i = 0; i < paralaxObjects.length; i++){
+    paralaxObjects[i].style.backgroundPositionY =  (i * 300) - window.scrollY/Math.max(1.3, i+1) + "px";
+  }
+}
