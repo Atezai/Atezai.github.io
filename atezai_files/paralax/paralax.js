90567fa7f744f66926354010430d34a658d4188b
Atezai <theatezai@gmail.com>
2019-04-08 12:41:02 +0200

    Version 2
    added project box gallery

--- atezai_files/paralax/paralax.js
+++ atezai_files/paralax/paralax.js
@@ -1,17 +1,9 @@
 var paralaxObjects = document.getElementsByClassName("paralax");
 
-InitializeParalaxEffect();
-
-function InitializeParalaxEffect(){
-  for(var i = 0; i < paralaxObjects.length; i++){
-    paralaxObjects[i].style.backgroundPositionX = i*i*500 + "px";
-    paralaxObjects[i].style.opacity = 1 - i/9;
-  }
-  ParalaxEffect();
-}
-
 function ParalaxEffect(){
   for(var i = 0; i < paralaxObjects.length; i++){
-    paralaxObjects[i].style.backgroundPositionY =  (i * 300) - window.scrollY/Math.max(1.3, i+1) + "px";
+    paralaxObjects[i].style.opacity = 1 - i/9;
+    paralaxObjects[i].style.backgroundPositionX = i*i*300 + "px";
+    paralaxObjects[i].style.backgroundPositionY =  (i * 300) - window.scrollY/Math.max(1.3, i+1) + "px"; 
   }
 }
