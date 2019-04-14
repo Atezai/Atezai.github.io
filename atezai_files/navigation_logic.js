801bd3ebd9e51130e0d76a5014779ba982357762
Atezai <theatezai@gmail.com>
2019-04-14 20:36:31 +0200

    v4

--- atezai_files/navigation_logic.js
+++ atezai_files/navigation_logic.js
@@ -1,5 +1,6 @@
 var nav_element = document.getElementById("Navigation");
-var nav_button_element = document.getElementById("Navigation_Button");
+var nav_button_open = document.getElementById("Navigation_Open");
+var nav_button_close = document.getElementById("Navigation_Close");
 var nav_opened = false;
 
 function NavButtonAction(){
@@ -11,15 +12,22 @@ function NavButtonAction(){
 }
 
 function ShowNav(){
-    nav_button_element.className = "nav-close-button";
+    nav_button_open.style.display = "none";
+    nav_button_close.style.display = "block";
     nav_element.className = "fullscreen-nav";
     document.body.style.height = "10vh";
+    document.body.style.overflow = "hidden";
     nav_opened = true;
 }
 
 function HideNav(){
-    nav_button_element.className = "nav-button";
+    if(window.innerWidth >= 1400)
+        nav_button_open.style.display = "none";
+    else
+        nav_button_open.style.display = "block";
+    nav_button_close.style.display = "none";
     nav_element.className = "";
     document.body.style.height = "auto";
+    document.body.style.overflow = "visible";
     nav_opened = false;
 }
